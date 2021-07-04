14135fde8e017bfd8d148899f2b431f2167aae6e
Kamil Berdychowski <kamil.berdychowski@vonage.com>
2021-07-04 18:24:51 +0200

    Client that retries when failure occurs

---
+++ client/src/main/java/pl/kamil/reactorplayground/client/RetryClient.java
@@ -0,0 +1,58 @@
+package pl.kamil.reactorplayground.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
+import java.util.UUID;
+
+@Component
+public class RetryClient implements ApplicationListener<ApplicationReadyEvent> {
+  private static final Logger log = LoggerFactory.getLogger(RetryClient.class);
+  private final WebClient webClient;
+
+  public RetryClient(WebClientFactory webClientFactory) {
+
+    this.webClient = webClientFactory.create()
+        .baseUrl("http://error-service/retry")
+        .build();
+  }
+
+  public Mono<String> greet(UUID uuid) {
+    return this.webClient.get()
+        .uri(b -> b.queryParam("uid", uuid.toString()).build())
+        .retrieve()
+        .bodyToMono(String.class)
+        .retryWhen(retryPolicy());
+  }
+
+  @Override
+  public void onApplicationEvent(ApplicationReadyEvent event) {
+    var uuid = UUID.randomUUID();
+    greet(uuid)
+        .doOnSubscribe(s -> log.info("Getting greeting for " + uuid))
+        .subscribe(
+            greeting -> log.info("Received greeting: " + greeting),
+            throwable -> log.error("Error while getting greeting", throwable));
+  }
+
+  private Retry retryPolicy() {
+    // error-service returns greeting after 2 attempts, so we are setting retry to 3
+    // we are logging each attempt
+    // Retry.backoff builds exponential backoff strategy
+    return Retry.backoff(3, Duration.ofMillis(100))
+        .maxBackoff(Duration.ofSeconds(1))
+        .doBeforeRetry(this::logAttempt);
+  }
+
+  private void logAttempt(Retry.RetrySignal retrySignal) {
+    var totalRetries = retrySignal.totalRetries();
+    log.info("Retry number " + totalRetries);
+  }
+}
